bb592df94ff24616e8a1bd43710419b803c17d32
unknown <daveasmah@yahoo.com>
2022-07-31 17:35:20 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+/*Create an object, then write a JavaScript function that checks 
+whether an object contains the specified key. */
+
+let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } 
+    }
+    }  
+}              
